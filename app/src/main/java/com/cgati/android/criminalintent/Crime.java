a5c5ab2c15502421f4d021a10ff904d3bd13a784
Christian Gati <christian.gati@gmail.com>
2015-02-05 00:55:08 -0500

    EditText Title Fragment

---
+++ app/src/main/java/com/cgati/android/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package com.cgati.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by cgati on 2/5/15.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
